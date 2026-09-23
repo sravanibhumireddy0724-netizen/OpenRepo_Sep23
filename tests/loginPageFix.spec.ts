b1433ee30d20e4f676afa2594afe7c44f6949bf6
Sravani <sravanibhumireddy2407@gmail.com>
2026-09-23 10:57:16 +0530

    changed to check GITUB SECRET

--- tests/loginPageFix.spec.ts
+++ tests/loginPageFix.spec.ts
@@ -4,7 +4,7 @@ import { ExcelHelper } from '../src/utils/ExcelHelper';
 import { JsonHelper } from '../src/utils/JsonHelper';
 
 
-test('Checking Config file reading - User able to login@regression',async({loginPage,homePage})=>{
+test('Checking Config file reading the GITHUB creds- User able to login@regression',async({loginPage,homePage})=>{
 await loginPage.goToLoginPage();
 await loginPage.doLogin(process.env.APP_USERNAME!,process.env.APP_PASSWORD!);
 await homePage.isLogoutLinkExist();
